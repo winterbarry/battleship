56fd72e5c4570784beb6d4c3db55701265a5b714
Obe Ajilima <ajilimaobe@gmail.com>
2026-01-27 12:51:22 +0100

    added gameboard class for testing

--- board.js
+++ board.js
@@ -1,3 +1,34 @@
-export default class Gameboard {
+import Ship from './ship.js'
 
+export default class Gameboard {
+    constructor(size = 10) {
+        this.size = size;
+        this.board = Array(size).fill(null).map(() => Array(size).fill(null)); 
+        this.ships = []; 
+        this.missedAttacks = [];
+    }
+
+    placeShip([row, col], length) {
+        const newShip = new Ship(length);
+
+        for (let i = 0; i < length; i++) {
+            this.board[row][col + i] = newShip; 
+        }
+
+        this.ships.push(newShip);
+    }
+
+    receiveAttack([row, col]) {
+        const target = this.board[row][col];
+
+        if (target instanceof Ship) {
+            target.hit();
+        } else {
+            this.missedAttacks.push([row, col]);
+        }
+    }
+    
+    allShipsSunk() {
+        return this.ships.every(ship => ship.isSunk());
+    }
 }
